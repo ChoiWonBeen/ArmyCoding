8ed7006ecb0ad9c2fcf57fcf90934ab32c153e17
WonBeen Choi <50780281+ChoiWonBeen@users.noreply.github.com>
2021-03-26 07:53:49 +0900

    Update slce.js

--- slce.js
+++ slce.js
@@ -1,6 +1,6 @@
 function showCode(){
   let htmlCode = document.getElementById("html-editor").value;
-  let jsCode = "<script>" + document.getElementById("js-editor").value + </script>";
+  let jsCode = "<script>" + document.getElementById("js-editor").value + "</script>";
   let cssCode = "<style>" + document.getElementById("css-editor").value + "</style>";
   console.log("html: " + htmlCode + "\njs: " + jsCode + "\ncss: " + cssCode);
   
