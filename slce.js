bccb2aaae524f00dddf4dd4e1942d24cf07481d0
WonBeen Choi <50780281+ChoiWonBeen@users.noreply.github.com>
2021-03-28 12:59:33 +0900

    Update slce.js

--- slce.js
+++ slce.js
@@ -1,7 +1,11 @@
+let HTMLEditor = document.getElementById("html-editor");
+let JSEditor = document.getElementById("js-editor")
+let CSSEditor = document.getElementById("css-editor")
+
 function showCode(){
-  let htmlCode = document.getElementById("html-editor").value;
-  let jsCode = "<script>" + document.getElementById("js-editor").value + "</script>";
-  let cssCode = "<style>" + document.getElementById("css-editor").value + "</style>";
+  let htmlCode = HTMLEditor.value;
+  let jsCode = "<script>" + JSEditor.value + "</script>";
+  let cssCode = "<style>" + CSSEditor.value + "</style>";
 
   let frame = document.getElementById("preview-display").contentWindow.document;
   frame.open();
@@ -13,5 +17,19 @@ let type = {"HTML", "JS", "CSS"};
 let active = "HTML";
 
 function activeHTML(){
-  
+  HTMLEditor.style.display = "block"
+  JSEditor.style.display = "none"
+  CSSEditor.style.display = "none"
+}
+
+function activeJS(){
+  HTMLEditor.style.display = "none"
+  JSEditor.style.display = "block"
+  CSSEditor.style.display = "none"
+}
+
+function activeCSS(){
+  HTMLEditor.style.display = "none"
+  JSEditor.style.display = "none"
+  CSSEditor.style.display = "block"
 }
