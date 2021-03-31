6d71bf309eee499eae4508cf8f00178a9a740106
WonBeen Choi <50780281+ChoiWonBeen@users.noreply.github.com>
2021-03-31 09:06:25 +0900

    Update slce.js

--- slce.js
+++ slce.js
@@ -19,7 +19,7 @@ changeType(0);
 function showCode(){
   let htmlCode = HTMLEditor.innerText;
   let jsCode = "<script>" + JSEditor.innerText + "</script>";
-  let cssCode = "<style>" + CSSEditor.value + "</style>";
+  let cssCode = "<style>" + CSSEditor.innerText + "</style>";
 
   let frame = document.getElementById("preview-display").contentWindow.document;
   frame.open();
@@ -52,7 +52,7 @@ function changeType(type){
 function saveCode() {
   localStorage.setItem("htmlCode", HTMLEditor.innerText);
   localStorage.setItem("jsCode", JSEditor.innerText);
-  localStorage.setItem("cssCode", CSSEditor.value);
+  localStorage.setItem("cssCode", CSSEditor.innerText);
 }
 
 window.onbeforeunload = function() {
