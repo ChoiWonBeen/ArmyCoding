b239033125617d187243421257bdccbd9c4043c8
WonBeen Choi <50780281+ChoiWonBeen@users.noreply.github.com>
2021-04-08 22:45:49 +0900

    Update slce.js

--- slce.js
+++ slce.js
@@ -29,7 +29,7 @@ editors.map((editor) => {
       let back = node.data.slice(end, node.length);
       node.data = front + "  " + back;
       
-      node.setSelectionRange(start + 2, start + 2);
+      console.log(node);
     }
   });
   
